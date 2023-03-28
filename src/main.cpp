e3d57588002ca20363d52e299ba9371cd18d20e7
Reece Mungal <reece.mungal.dev@hotmail.com>
2023-03-28 18:49:58 -0400

    Added initial example bluetooth code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,53 @@
 #include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "ELMduino.h"
+
+
+BluetoothSerial SerialBT;
+#define ELM_PORT   SerialBT
+#define DEBUG_PORT Serial
+
+
+ELM327 myELM327;
+
+
+uint32_t rpm = 0;
 
 void setup() {
   // put your setup code here, to run once:
+#if LED_BUILTIN
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+#endif
+
+  DEBUG_PORT.begin(115200);
+  //SerialBT.setPin("1234");
+  ELM_PORT.begin("ArduHUD", true);
+  
+  if (!ELM_PORT.connect("OBDII"))
+  {
+    DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
+    while(1);
+  }
+
+  if (!myELM327.begin(ELM_PORT, true, 2000))
+  {
+    Serial.println("Couldn't connect to OBD scanner - Phase 2");
+    while (1);
+  }
+
+  Serial.println("Connected to ELM327");  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+float tempRPM = myELM327.rpm();
+
+  if (myELM327.nb_rx_state == ELM_SUCCESS)
+  {
+    rpm = (uint32_t)tempRPM;
+    Serial.print("RPM: "); Serial.println(rpm);
+  }
+  else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+    myELM327.printError();  
 }
